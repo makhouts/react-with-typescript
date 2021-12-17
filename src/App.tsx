9c8300c774c327f9e7e1edf3acfb9c6ff72540a9
Sofiane  Makhout <sofiane@Sofianes-MacBook-Air.local>
2021-12-17 09:52:52 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+interface IState {
+  people: {
+    name: string,
+    age: number,
+    url: string,
+    note?: string
+  }[]
+}
+
+function App() {
+  const [people, setPeople] = useState<IState['people']>([])
+  
+  return (
+    <div className="App">
+      <h1>People invited to my party</h1>
+    </div>
+  );
+}
+
+export default App;
